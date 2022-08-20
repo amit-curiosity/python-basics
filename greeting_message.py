6d2ac6e7132cddd8cb4a07f1cdaaa4eecb7f1f92
amit-curiosity <amit.curiosity@gmail.com>
2022-08-20 11:26:32 +0530

    "password_generator.py - file_added"

---
+++ greeting_message.py
@@ -0,0 +1,8 @@
+def greeting_message(first_name, last_name):
+    print(f'Hi {first_name} {last_name}!')
+
+
+if __name__ == '__main__':
+    first_part_of_name: str = 'Amit'
+    last_part_of_name: str = 'Shaw'
+    greeting_message(first_part_of_name, last_part_of_name)
