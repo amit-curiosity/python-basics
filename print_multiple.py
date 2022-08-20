1e8338f41484b6ad90abd480b50db57eddb45e45
amit-curiosity <amit.curiosity@gmail.com>
2022-08-20 11:12:11 +0530

    "print_multiple.py - file_added"

---
+++ print_multiple.py
@@ -0,0 +1,13 @@
+def print_multiple():
+    number = int(input("Enter a number: "))
+    i = 1
+    while i <= 10:
+        # "<3" - left align of field_width 3
+        # ">3" - right align of field_width 3
+        # "^4" - center align of field_width 4
+        print(f'{number: <3} * {i: >3} = {(i * number): ^4}')
+        i += 1
+
+
+if __name__ == '__main__':
+    print_multiple()
