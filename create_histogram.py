8928736c3dfd84dad752f7c5e922f6ebbac75077
amit-curiosity <amit.curiosity@gmail.com>
2022-08-20 10:48:09 +0530

    "create_histogram - file_modified"

---
+++ create_histogram.py
@@ -0,0 +1,12 @@
+# define histogram creation logic
+def create_histogram(list_of_numbers):
+    for number in list_of_numbers:
+        print('|', end='')
+        for _ in range(number):
+            print('*-', end='')
+        print('*')
+
+
+if __name__ == '__main__':
+    numbers = [7, 6, 8, 9, 1, 5, 12]
+    create_histogram(numbers)
