be2c1288a766f5d65dae5f63dcea40a33690abc6
amit-curiosity <amit.curiosity@gmail.com>
2022-08-20 11:39:42 +0530

    "list_files.py - file_added"

---
+++ list_files.py
@@ -0,0 +1,17 @@
+import os
+
+# get current file's path
+path = os.path.realpath(__file__)
+
+# get current_file's directory
+dir_name = os.path.dirname(path)
+
+# print(os.listdir(dir_name))
+
+# go through directory's files & folders
+for file_name in os.listdir(dir_name):
+    file, ext = os.path.splitext(file_name)
+
+    # print files with python extension
+    if ext == '.py':
+        print(file_name)
